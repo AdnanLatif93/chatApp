a6a06fa2681e4f7768f091e4a24061137bdbfa19
Adnan Faisal Latif <adnan.faisal@postex.pk>
2022-10-25 16:31:23 +0500

    add angular material and cdk w.r.t angular version

--- chatApp/src/app/app.component.ts
+++ chatApp/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatApp';
+  constructor(
+    private dialog : MatDialog
+  ){
+    
+  }
 }
